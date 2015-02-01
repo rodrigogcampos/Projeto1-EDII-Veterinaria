789bc44b41dbf965d8f345e0797d14870b8221ce
rodrigogcampos <rodrigo.guicampos@gmail.com>
2015-02-01 02:30:00 -0200

    Arquivo principal 2

--- Principal.c
+++ Principal.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct key1{
+	int cod;
+	int position;
+}ind1;
+
+struct key2{
+	char vacine[15];
+	int position;
+}ind2;
+
 struct animal{
     int cod;
     char raca[15];
@@ -85,11 +95,12 @@ void sair(){
 
 int opcao1(){
 
-    int codexist;
+    int cod;
+    char name[15];
+    char raca[15];
+    
     char confirm[] = "S";
-    struct animal c;
-    int aux;
-    char aux2[10] = "";
+    
 
     while (strcmp ((strupr(confirm)), "S") == 0)
     {
@@ -98,94 +109,107 @@ int opcao1(){
         printf("\ncodigo: ");
         fflush(stdin);
         scanf("%d",&dog.cod);
-        sprintf(aux2, "%d", dog.cod);
-        printf("%s",aux2);
+       
+        printf("nome: ");
+       	fflush(stdin);
+        scanf("%s",dog.name);
 
-        /*codexist = 0;
-        while(!feof(AP2))
-        {
-            fread(&c,sizeof(dog),1,AP2);
-            //printf("%d\n%s\n%s\n", c.cod, c.name, c.raca);
-            if (dog.cod == c.cod){
-                codexist = 1;
-                break;
-            }
-        }*/
+        printf("raca: ");
+        fflush(stdin);
+        scanf("%s",dog.raca);
+        
+        fwrite(&dog.cod,sizeof(dog.cod),1,AP2);
+        fwrite(dog.name,sizeof(dog.name),1,AP2);
+        fwrite(dog.raca,sizeof(dog.raca),1,AP2);
+        
         fclose(AP2);
-
-        if (codexist == 1)
-        {
-            printf("codigo do dog ja exite!\n");
-            printf("Cadastrar novamente?[S/N]");
-            gets(confirm);
-        }
-        else{
-            printf("nome: ");
-            fflush(stdin);
-            gets(dog.name);
-
-            printf("raca: ");
-            fflush(stdin);
-            gets(dog.raca);
-
-            AP2 = fopen("AP2.bin", "ab");
-           // fputc(&aux2,AP2);
-            fwrite(&aux2,sizeof(aux2),1,AP2);
-            fwrite(&dog.name,sizeof(dog.name),1,AP2);
-            fwrite(&dog.raca,sizeof(dog.raca),1,AP2);
-            fclose(AP2);
-
-            printf("Deseja cadastrar outro cachorro?[S/N]");
-            gets(confirm);
-        }
-
+        
+        printf("Deseja cadastrar outro cachorro?[S/N]");
+        scanf("%s",confirm);
     }
     sair();
 }
-int opcao2(){ /* não precisa
-    FILE *BKP;
-    struct animal c[1000];
-
-    AP2 = fopen("AP2.txt", "rb");
-    BKP = fopen("BKP.txt", "w");
-    int i=1;
-
-    while(!feof(AP2))
-    {
-        fread(&c[i],sizeof(dog),1,AP2);
-
-        printf("%s\n%s\n%s\n", c[i].cod, c[i].name, c[i].raca);
-        printf("\n***%d***\n",i);
-        i++;
-    }
-    fwrite(&c[1],sizeof(dog),i-1,BKP);
-
-    fclose(AP2);
-    fclose(BKP);
-
-    sair();*/
+int opcao2(){ 
+   
 }
-int opcao3(){
-        AP1 = fopen("AP1.txt", "wb");
-        char registro[35];
 
-        printf("Nome da vacina: ");
+void inivec(FILE *AP1){
+	
+	AP1 = fopen("AP1.bin","rb");
+	
+	//char (*vetor[2])[20];
+	char c[1];
+	int i;
+	
+//	while(!feof){
+		fread(c,sizeof(c),1,AP1);
+		//c = getc(AP1);
+		//i++;
+		printf("%x",c);
+	//}
+//	i = 0;
+//	while(!feof){
+		//printf("%s", vetor[i]);
+		//i++;
+	//}
+	
+}
+
+int opcao3(){
+        AP1 = fopen("AP1.bin", "ab");
+        char buffer[100];
+        char aux[100];
+        int size;
+        
+        strcat(buffer,"!");
+        
+		printf("Codigo de controle: ");
         fflush(stdin);
-        gets(vacina.name);
+        scanf("%d",&vacina.codcontrol);
+        itoa(vacina.codcontrol,aux,10);
+        strcat(buffer,aux);
+        strcat(buffer,"|");
+        
+        printf("Codigo do cachorro: ");
+        fflush(stdin);
+        scanf("%d",&vacina.coddog);
+        itoa(vacina.coddog,aux,10);
+        strcat(buffer,aux);
+        strcat(buffer,"|");
+        		
+		printf("Nome da vacina: ");
+        fflush(stdin);
+        scanf("%s",vacina.name);
+        strcat(buffer,vacina.name);
+        strcat(buffer,"|");
 
         printf("Data da vacina: ");
-        gets(vacina.date);
+        scanf("%s",vacina.date);
+        strcat(buffer,vacina.date);
+        strcat(buffer,"|");
 
         printf("Responsavel pela vacina: ");
-        gets(vacina.person);
-
-        sprintf(registro,"%s|%s|%s|",vacina.name,vacina.date,vacina.person);
-        fwrite(registro,sizeof(registro),1,AP1);
-
+        scanf("%s",vacina.person);
+        strcat(buffer,vacina.person);
+        strcat(buffer,"|");
+        
+        printf("%s", buffer);
+        
+        size = strlen(buffer);
+    	
+		fprintf(AP1,"%d",size);
+        fprintf(AP1,"%s",buffer);
+       
         fclose(AP1);
+        
+        printf("\n*********************************\n");
+        inivec(AP1);
+        printf("\n*********************************\n");
 
     sair();
 }
+
+
 int opcao4(){
     printf("4444444444444444444444444444444\n");
     sair();
@@ -199,7 +223,7 @@ int opcao6(){
     sair();
 }
 int opcao7(){
-    printf("6666666666666666666666666666666\n");
+   	
     sair();
 }
 int opcao8(){
@@ -213,12 +237,14 @@ int main() {
     if (AP2 == NULL){
         fopen("AP1.bin", "w");
     }
+	fclose(AP1);
 
-    AP2 = fopen("AP2.bin", "a");
+   	AP2 = fopen("AP2.bin", "a");
     if (AP2 == NULL){
         fopen("AP2.bin", "w");
     }
     fclose(AP2);
+    
     menu();
     return 0;
 }
